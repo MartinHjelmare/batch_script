5166722c75277dcc0d93ef55669502c1c6764e70
MartinHjelmare <marhje52@kth.se>
2013-12-05 17:55:11 +0100

    Testing

--- test4.sh
+++ test4.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-well="B1"
+well="C1"
 start_letter=${well:0:1}
 well_letters="ABCDEFGH"
-well_letters="${$well_letters/"*"$start_letter/$start_letter}"
+well_letters="${well_letters/*$start_letter/$start_letter}"
 
 while test -n "$well_letters"; do
     well_letter=${well_letters:0:1}     # get the first character in well_letters
     echo character is $well_letter
     well_letters=${well_letters:1}   # trim the first character in well_letters
 done
-
-fov_name="${filename%"${fileend}"}"
