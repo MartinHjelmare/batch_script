e1a58cfb3b0cde2c94d561f9792f6e92b381628d
MartinHjelmare <marhje52@kth.se>
2013-12-05 03:18:07 +0100

    Almost finished testing. Need to fix well traversal.

--- confocalanalyze.sh
+++ confocalanalyze.sh
@@ -1,35 +1,95 @@
 #!/bin/bash
 
+# $1 is resolution, $2 is input folder, $3 is output folder, $4 is start plate, $5 is start well, $6 is end plate, $7 is remote host path.
+
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+well=$5
 
-#rsync -rav --include '*/' --include='1054_G12_1*.tif.gz' --exclude='*' \
-#/home/martin/Skrivbord/feature_extract_test/1054/ /home/martin/Skrivbord/feature_extract/incoming/core2
+#Make for loop, runs for specified plates ($plate) and wells ($well), from $4 and $5 to $6.
+for (( plate=${4}; plate<=${6}; plate++ ))
+do  
+    well_letters="ABCDEFGH"
+    
+    while test -n "$well_letters"; do
+        well_letter=${well_letters:0:1}     # get the first character in well_letters
+        echo character is $well_letter
+        well_letters=${well_letters:1}   # trim the first character in well_letters
+    done
+    
+    echo $plate $well #testing
 
-#rsync -rav --include '*/' --include='1054_G12_10*.tif.gz' --exclude='*' \
-#/home/martin/Skrivbord/feature_extract_test/1054/ /home/martin/Skrivbord/feature_extract/incoming/core2
+    mkdir $2/$plate
 
-fullfile="$(find $1 -name '1054*_blue*')"
-filename="${fullfile##*/}"
-fileend="${fullfile##*_}"
-fov_name="${filename%"${fileend}"}"
-rm -rf $dir/tmp/siIn
-rm -rf $dir/tmp/siOut
-mkdir -p $dir/tmp/siIn/in
-mkdir -p $dir/tmp/siOut
-gzip -d $1/*.gz
-ln -s -t $dir/tmp/siIn/in $1/*tif
-if [ "$2" == "10" ]
-	then
-		matlab -nodisplay -nodesktop -nojvm -nosplash -r "cd $dir/features; [arr exit_status] = process_10x('./tmp/siIn','./tmp/siOut'); exit(exit_status);"
-	else 
-		matlab -nodisplay -nodesktop -nosplash -r "cd $dir/features; [arr exit_status] = process_63x('$dir/tmp/siIn','$dir/tmp/siOut',$2); exit(exit_status);"
-fi
+    rsync -rav --include '*/' --include="$plate"_"$well*.tif.gz" --exclude='*' \
+    $7/$plate/ $2/$plate
+    #here
 
-mv ./tmp/siOut/segmentation.png $1/$fov_name"segmentation.png"
+    arr=( $(find $2/$plate -type f | sort) )
+    echo ${#arr[@]} #testing
 
-if [ -f $dir/tmp/siOut/features.csv ];
-then
-	mv $dir/tmp/siOut/features.csv $1/$fov_name"features.csv"
-else
-	exit 1
-fi
+    # While loop, runs as long as there are files left in ../$2/$plate
+    while [ ${#arr[@]} -ge 1 ]
+    do
+
+        # Find fov_name
+        fullfile="${arr[0]}"
+        filename="${fullfile##*/}"
+        fileend="${fullfile##*_}"
+        fov_name="${filename%"${fileend}"}"
+        echo $fov_name #testing
+
+        rm -rf $dir/tmp/siIn
+        rm -rf $dir/tmp/siOut
+        mkdir -p $dir/tmp/siIn/in
+        mkdir -p $dir/tmp/siOut
+        
+        gunzip -d $2/$plate/$fov_name*.gz
+
+        ln -s -t $dir/tmp/siIn/in $2/$plate/$fov_name"blue.tif"
+        ln -s -t $dir/tmp/siIn/in $2/$plate/$fov_name"green.tif"
+        ln -s -t $dir/tmp/siIn/in $2/$plate/$fov_name"red.tif"
+        ln -s -t $dir/tmp/siIn/in $2/$plate/$fov_name"yellow.tif"
+
+        echo $dir/tmp/siIn/in #testing
+
+        # Check to make sure the links to four different color images exists in ../tmp/siIn/in, if it does, then run matlab on them
+        links="$( find "$dir/tmp/siIn/in" -type l )"
+         
+        for link in $links; do
+            echo $link #testing
+            if [ ! -e $link ] ; then
+                images_exist=0
+                break
+            else
+                images_exist=1
+            fi
+        done
+
+        if [ "$1" == "10" ] && [ "$images_exist" == "1" ]
+        then
+        	matlab -nodisplay -nodesktop -nojvm -nosplash -r "cd $dir/features; [arr exit_status] = process_10x('./tmp/siIn','./tmp/siOut'); exit(exit_status);"
+        elif [ "$images_exist" == "1" ]
+        then
+        	echo -nodisplay -nodesktop -nosplash -r "cd $dir/features; [arr exit_status] = process_63x('$dir/tmp/siIn','$dir/tmp/siOut',$1); exit(exit_status);"
+        else
+            echo "no images to process"
+        fi
+
+        if [ "$images_exist" == "1" ]
+        then
+            mv ./tmp/siOut/segmentation.png $3/$plate/$fov_name"segmentation.png"
+
+            if [ -f $dir/tmp/siOut/features.csv ];
+            then
+            	mv $dir/tmp/siOut/features.csv $3/$plate/$fov_name"features.csv"
+            else
+            	exit 1 #temporary testing without exit
+            fi
+        fi
+
+        rm -f $2/$plate/$fov_name*
+        arr=( $(find $2/$plate -type f) )
+        echo ${#arr[@]} #testing
+    done
+done
+
---
+++ launch_job_feature_extract.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH -A snic2013-1-198
+#SBATCH -p node -n 8
+#SBATCH -t 2-00:00:00
+#SBATCH -J feature_extract_1-32
+
+# At Uppmax:
+# Save incoming images in $HOME/glob (250 GB quota).
+# Create image links and temporary files in path that $SNIC_TMP variable is pointing at, equal to path /scratch/$SLURM_JOB_ID .
+# Make sure you create $HOME/glob/feature_extract/incoming/ and $HOME/glob/feature_extract/output before running this script.
+
+module load matlab #Load module matlab
+
+mkdir $SNIC_TMP/feature_extract/
+cp $HOME/glob/feature_extract/confocal_feature.zip $SNIC_TMP/feature_extract/
+cd $SNIC_TMP/feature_extract/
+unzip feature_extract.zip
+
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ $HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+wait
