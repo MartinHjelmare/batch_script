334e2e955b68422e7c9c10e533565cc2a4299660
MartinHjelmare <marhje52@kth.se>
2015-04-17 14:46:39 +0200

    Added more comments and corrected arguments as to work with called script

--- launch_job_feature_extract.sh
+++ launch_job_feature_extract.sh
@@ -6,11 +6,31 @@
 #SBATCH --mail-type=ALL
 
 # At Uppmax:
+
+# SLURM settings:
+# Replace snic2013-1-198 with your project name.
+# Specify number of nodes or cores to use.
+# Specify max time
+# Specify job name
+# Make sure you get notified by mail, when stuff happens.
+
+
 # Save incoming images in $HOME/glob (250 GB quota).
 # Create image links and temporary files at $SNIC_TMP,
 # equal to path /scratch/$SLURM_JOB_ID .
 # Make sure you create $HOME/glob/feature_extract/incoming/
 # and $HOME/glob/feature_extract/output before running this script.
+
+# Replace plate_start and plate_end with line numbers representing start and end
+# for reading from txt_file. Replace txt_file with path to a text file
+# containing a plate number on each line. The file will be read from plate_start
+# to plate_end and the images from those plates or folders will be used as
+# input.
+
+# Each node, eight in this case, will have its own script running. Accomplish
+# this by using "&" between script calls, as below.
+
+# Remember to load modules first, to be able to call programs, like matlab.
 
 module load matlab #Load module matlab
 
@@ -19,22 +39,22 @@ cp $HOME/glob/feature_extract/feature_extract.zip $SNIC_TMP/feature_extract/
 cd $SNIC_TMP/feature_extract/
 unzip feature_extract.zip
 
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
-./confocalanalyze.sh 63 $HOME/glob/feature_extract/incoming/ \
-$HOME/glob/feature_extract/output plate_start well_start plate_end remote_host &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
+./confocalanalyze_batch.sh 63 $HOME/glob/feature_extract/incoming/ \
+$HOME/glob/feature_extract/output plate_start plate_end txt_file &
 wait
 rm -rf $SNIC_TMP/feature_extract/
 echo "Job finished successfully!"
